b087412a781a60cb178efd6b36a88d6c4d503a69
h64 <hhongnr@gmail.com>
2021-02-02 09:00:38 -0800

    Substantial refactor - Feature addition: Can pull from two github orgs

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ To clone your student's pull requests from github.com/WDI-SEA/css-positioning:
 `node cloneHw.js css-positioning`
 
 ### Command Line Output: 
-![cli](https://i.imgur.com/xO6kaDr.png)
+![cli](https://i.imgur.com/iWJS5RI.png)
 
 ### students.json:
 `students.json` example:
@@ -23,3 +23,7 @@ Node
 1. Fix crash when user's repo name is different from upstream repo name
 2. Fix crash on invalid args
 3. Fix script ending early on re-clone 
+
+
+### Feature Additions:
+1. Tool now can pull from multiple Github organizations simultaneously 
--- cloneHw.js
+++ cloneHw.js
@@ -2,72 +2,106 @@ const https = require("https");
 const { spawn } = require("child_process");
 const { students } = require("./students.json");
 
+const orgs = ['SEI-SEA-1-25', 'WDI-SEA'] // Adjust Github orgs list here
 const repoName = process.argv[2];
 
-// If no repo specified in cli args, end
+// If no repo specified in cli args, end program
 if (process.argv.length === 0) {
     console.log("No repository name supplied in arguments! Exiting...");
     process.exit();
 }
 
-// options object
-const options = {
-    hostname: "api.github.com",
-    path: `/repos/SEI-SEA-1-25/${repoName}/pulls`,
-    method: "GET",
-    headers: {
-        "User-Agent": "h64"
-    }
-};
+async function main() {
+    let pullRequests = await Promise.all(orgs.map(org => xhr(org)))
+    pullRequests = [].concat.apply([], pullRequests); // Flatten array of arrays
+
+    let studentSubmissions = [];
+    studentSubmissions = getStudentsPullRequests(pullRequests);
+    await cloneRepositories(studentSubmissions)
+    logMissingSubmissions(studentSubmissions);
+}
 
 // make xhr request
-https.get(options, res => {
-    res.setEncoding("utf8");
-    let body = "";
-
-    res.on("data", data => {
-        body += data;
-    });
-    res.on("end", () => {
-        body = JSON.parse(body);
-        if (body.message) {
-            console.error(`Error: Invalid repository: WDI-SEA/${repoName}`);
-            process.exit(1);
+async function xhr(org) {
+    const options = {
+        hostname: "api.github.com",
+        path: `/repos/${org}/${repoName}/pulls`,
+        method: "GET",
+        headers: {
+            "User-Agent": "h64"
         }
-        cloneRepositories(getSubmittors(body));
-    });
-    res.on("error", err => {
-        console.error("Error:", err);
-    });
-});
+    };
+    return new Promise((resolve, reject) => {
+        https.get(options, res => {
+            res.setEncoding("utf8");
+            let body = "";
+        
+            res.on("data", data => {
+                body += data;
+            });
+            res.on("end", () => {
+                body = JSON.parse(body);
+                if (body.message) {
+                    console.error(`Warning: No repository found for: ${org}/${repoName}`);
+                }
+                resolve(body);
+            });
+            res.on("error", err => {
+                reject(new Error('Failed XHR, status code: ', res.statusCode))
+            });
+        });
+    })
+}
+
 
 // find which students made submissions, and return that list as array
-function getSubmittors(input) {
+function getStudentsPullRequests(pullRequests) {
     const submissions = [];
     const usernames = students.map(({ username }) => username);
-    for (pullRequest of input) {
+    for (pullRequest of pullRequests) {
+        // If this student already submitted HW in multiple orgs - don't include dupe submission from other org
+        // This also implicitly makes a preference on one PR over another in the case of dupes
+        // The chance of this actually coming up and being a problem is very small - just clone it manually at that point
+        let username = pullRequest.user.login;
+        if (submissions.indexOf(username) > -1) { continue } ; 
+        // disallow PR from branches
+        if (pullRequest.head.ref !== 'main' && pullRequest.head.ref !== 'master' ) { continue } ; 
+        
         let idx = usernames.indexOf(pullRequest.user.login);
         if (idx > -1) {
             // PR confirmed submitted - add the student to the 'submitted' list
-            let submissionDetails = students[idx];
-            submissionDetails.repoPath = pullRequest.head.repo.full_name;
-            submissions.push(submissionDetails);
+            // let submissionDetails = students[idx];
+            // submissionDetails.repoPath = pullRequest.head.repo.full_name;
+            submissions.push(pullRequest);
         }
     }
     return submissions;
 }
 
 // from the list, clone repositories
-function cloneRepositories(submissions) {
-    var cliCommand = `mkdir -p ${repoName} && cd ${repoName}; `; // mkdir if doesn't exist and cd
+async function cloneRepositories(submissions) {
+    // BASH scripting magic: If folder not present - mkdir and clone, else echo message
+    // $1: studentName, $2: repoPath, $3: orgName
+    let bashFunction = 
+    `hw() { 
+        if [ ! -d "$1" ] ; then
+            mkdir "$1";
+            git clone -q git@github.com:$2.git $1;
+            echo "Cloning into '${repoName}/$1' from $3"
+        else
+            echo "$1: directory already on drive"
+        fi
+    }`
+
+    // mkdir if doesn't exist and cd
+    var cliCommand = `${bashFunction} && mkdir -p ${repoName} && cd ${repoName}`; 
     submissions.forEach(submission => {
-        cliCommand +=
-            // if folder not present - mkdir and clone, else echo message
-            `[ ! -d "${submission.name}" ] && 
-                mkdir "${submission.name}" && git clone git@github.com:${
-                submission.repoPath
-            }.git ${submission.name} ||
-                echo "${submission.name}: directory already on drive";`;
+        let githubUsername = submission.user.login;
+        let studentName = students.find(student => student.username == githubUsername).name;
+        let repoPath = submission.head.repo.full_name;
+        let orgName = submission.base.user.login;
+
+        cliCommand += ` && hw ${studentName} ${repoPath} ${orgName}`
     });
 
     const childProcess = spawn(cliCommand, { shell: true });
@@ -78,28 +112,22 @@ function cloneRepositories(submissions) {
     childProcess.stderr.on("data", data => {
         console.error(data.toString().trim());
     });
-    childProcess.on("exit", exitCode => {
-        console.log(""); //newline
-        logMissingSubmissionsReport(submissions);
-        console.log(
-            `Done. ${submissions.length} / ${
-                students.length
-            } repositories cloned.`
-        );
-        // console.log("Child exited with code: " + exitCode);
+    const exitCode = await new Promise((resolve, reject) => {
+        childProcess.on('close', resolve);
     });
 }
 
+
 // print out which students didn't submit pull request submission
-function logMissingSubmissionsReport(submissions) {
-    if (submissions.length === students.length) {
-        return;
-    } else {
-        let difference = students.filter(
-            student => !submissions.includes(student)
-        );
+function logMissingSubmissions(submissions) {
+    if (submissions.length !== students.length) {
+        let githubUsernames = submissions.map(submission => submission.user.login) //array of github usernames that made submission
+        let difference = students.filter(student => !githubUsernames.includes(student.username)); //array of students that didnt make submission
+
         let names = "";
         difference.forEach(student => (names += `${student.name} `));
         console.log(`Missing submissions from: ${names}`);
     }
 }
+
+main();
