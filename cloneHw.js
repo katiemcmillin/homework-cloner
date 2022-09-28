933ef643b7b37f099a129b614262c0a22a201cbe
weston-bailey <wdbailey87@gmail.com>
2022-09-28 15:30:40 -0700

    runns prettier

--- cloneHw.js
+++ cloneHw.js
@@ -1,20 +1,20 @@
-const https = require("https") 
+const https = require("https")
 const readLine = require('readline')
-const { spawn } = require("child_process") 
+const { spawn } = require("child_process")
 
-const { 
-  existsSync, 
-  readFileSync, 
-  writeFileSync 
+const {
+  existsSync,
+  readFileSync,
+  writeFileSync
 } = require('fs')
 
-const { 
-  students, 
+const {
+  students,
   githubToken,
   userName,
   hostname,
-  orgs 
-} = require("./config.json") 
+  orgs
+} = require("./config.json")
 
 // color escape codes
 const colors = {
@@ -38,58 +38,58 @@ const {
 } = colors
 
 // match command flags that start with --
-const flags = process.argv.filter(argv => argv.match(/(?<!\w)--\w+/)) 
+const flags = process.argv.filter(argv => argv.match(/(?<!\w)--\w+/))
 // to check if an org has the repo
 const orgChecks = orgs
 
 // the repo name should be the first arg to the script
 async function main(repoName = process.argv[2]) {
-	// If no repo specified in cli args, end program
-	if (!repoName) {
-		console.log(`${error('No arguments to evaluate!')}\nExiting...`)
-		process.exit()
-	}
+  // If no repo specified in cli args, end program
+  if (!repoName) {
+    console.log(`${error('No arguments to evaluate!')}\nExiting...`)
+    process.exit()
+  }
 
-	// check if the finished-assignments.json exists, if not, create it
-	if (!existsSync('./finished-assingments.json')) {
-		console.log(warn('./finished-assignments.json not found, creating it now...'))
-		createFinishedJson()
-	}
+  // check if the finished-assignments.json exists, if not, create it
+  if (!existsSync('./finished-assingments.json')) {
+    console.log(warn('./finished-assignments.json not found, creating it now...'))
+    createFinishedJson()
+  }
 
-	// forego cloning if one of the following flags is found
-	if (flags.includes('--check')) return checkSubmissions()
-	if (flags.includes('--forget')) return forgetRepo(repoName)
-	if (flags.includes('--list')) return listAssignments()
-	if (flags.includes('--sync')) return syncStudents()
-	if (flags.includes('--updateAll')) return updateAll()
+  // forego cloning if one of the following flags is found
+  if (flags.includes('--check')) return checkSubmissions()
+  if (flags.includes('--forget')) return forgetRepo(repoName)
+  if (flags.includes('--list')) return listAssignments()
+  if (flags.includes('--sync')) return syncStudents()
+  if (flags.includes('--updateAll')) return updateAll()
   if (flags.includes('--completed')) return completed(repoName, process.argv[3])
   if (flags.includes('--allCompleted')) return allCompleted(repoName)
 
-	return cloneHw(repoName)
+  return cloneHw(repoName)
 }
 
 async function cloneHw(repoName) {
   let pullRequests = await Promise.all(orgs.map(org => xhr(org, repoName)))
   // Flatten array of arrays
-  pullRequests = [].concat.apply([], pullRequests)  
+  pullRequests = [].concat.apply([], pullRequests)
   // Strip empty PR, in the case of no PR from 2nd org
-  pullRequests = pullRequests.filter(pr => pr.message != 'Not Found') 
+  pullRequests = pullRequests.filter(pr => pr.message != 'Not Found')
   // pullRequests.forEach(pullRequest => {
   //   console.log(pullRequest.number, pullRequest.user)
   // })
 
-  let studentSubmissions = [] 
-  studentSubmissions = getStudentsPullRequests(pullRequests) 
+  let studentSubmissions = []
+  studentSubmissions = getStudentsPullRequests(pullRequests)
   await cloneRepositories(studentSubmissions, repoName)
   // only update finished assignments if the --noTrack flag isn't found
-  if(!flags.includes('--noTrack')) {
+  if (!flags.includes('--noTrack')) {
     addNewAssigment(repoName)
     updateFinishedAssignments(studentSubmissions, repoName)
     console.log(info(`Tracking submissions for ${repoName}!`))
   } else {
     console.log(info(`Not tracking the submissions! for ${repoName}`))
   }
-  logMissingSubmissions(studentSubmissions) 
+  logMissingSubmissions(studentSubmissions)
 }
 
 // make xhr request
@@ -108,19 +108,19 @@ async function xhr(org, repoName) {
 
   return new Promise((resolve, reject) => {
     https.get(options, res => {
-      res.setEncoding("utf8") 
-      let body = "" 
-    
+      res.setEncoding("utf8")
+      let body = ""
+
       res.on("data", data => {
-        body += data 
-      }) 
+        body += data
+      })
       res.on("end", async () => {
-        body = JSON.parse(body) 
+        body = JSON.parse(body)
         if (body.message) {
           console.log(error(body.message))
           // if its not already been found to not have the repo
           if (orgChecks[orgs.indexOf(org)]) {
-          
+
             console.error(error(`Warning: No repository found for: ${org}/${repoName}`))
             orgChecks[orgs.indexOf(org)] = false
           }
@@ -132,12 +132,12 @@ async function xhr(org, repoName) {
           }
           // await quitOrContinue()
         }
-        resolve(body) 
-      }) 
+        resolve(body)
+      })
       res.on("error", err => {
         reject(new Error(error('Failed XHR, status code: '), res.statusCode))
-      }) 
-    }) 
+      })
+    })
   })
 }
 
@@ -175,27 +175,27 @@ async function quitOrContinue() {
 
 // find which students made submissions, and return that list as array
 function getStudentsPullRequests(pullRequests) {
-  const submissions = [] 
-  const usernames = students.map(({ username }) => username) 
+  const submissions = []
+  const usernames = students.map(({ username }) => username)
   for (pullRequest of pullRequests) {
     // If this student already submitted HW in multiple orgs - don't include dupe submission from other org
     // This also implicitly makes a preference on one PR over another in the case of dupes
     // The chance of this actually coming up and being a problem is very small - just clone it manually at that point
 
-    let username = pullRequest.user.login 
-    if (submissions.indexOf(username) > -1) { continue }   
+    let username = pullRequest.user.login
+    if (submissions.indexOf(username) > -1) { continue }
     // disallow PR from branches -- uncomment to skip all except specific branch names
     // if (pullRequest.head.ref !== 'main' && pullRequest.head.ref !== 'master' ) { continue }   
-    
-    let idx = usernames.indexOf(pullRequest.user.login) 
+
+    let idx = usernames.indexOf(pullRequest.user.login)
     if (idx > -1) {
       // PR confirmed submitted - add the student to the 'submitted' list
       // let submissionDetails = students[idx] 
       // submissionDetails.repoPath = pullRequest.head.repo.full_name 
-      submissions.push(pullRequest) 
+      submissions.push(pullRequest)
     }
   }
-  return submissions 
+  return submissions
 }
 
 // from the list, clone repositories
@@ -205,8 +205,8 @@ async function cloneRepositories(submissions, repoName) {
   // OG bash function that doesn't reclone when a folder is found
   let msg = warn('directory already on drive')
   const clonePath = hostname === 'api.github.com' ? 'github.com' : hostname
-  let bashFunction = 
-  `hw() { 
+  let bashFunction =
+    `hw() { 
     if [ ! -d "$1" ] ;  then
       mkdir "$1" 
       echo "Cloning into '${repoName}/$1' from $3"
@@ -217,12 +217,12 @@ async function cloneRepositories(submissions, repoName) {
   }`
 
   // deleted found repos if the --overWrite flag is used
-  if(flags.includes('--overWrite')) {
+  if (flags.includes('--overWrite')) {
     msg = warn('directory already on drive, deleting...')
     console.log(warn('overwriting existing cloned repos...'))
 
-    bashFunction = 
-    `hw() { 
+    bashFunction =
+      `hw() { 
       # check if folder exists
       if [ -d "$1" ] ; then
         echo "$1: ${msg}"
@@ -235,52 +235,52 @@ async function cloneRepositories(submissions, repoName) {
       git clone -q git@${clonePath}:$2.git $1 
     }`
   }
-  
+
   // mkdir if doesn't exist and cd
-  let cliCommand = `${bashFunction} && mkdir -p ${repoName} && cd ${repoName}`  
+  let cliCommand = `${bashFunction} && mkdir -p ${repoName} && cd ${repoName}`
   submissions.forEach(submission => {
-    let githubUsername = submission.user.login 
-    let studentName = students.find(student => student.username == githubUsername).name 
-    let repoPath = submission.head.repo.full_name 
-    let orgName = submission.base.user.login 
+    let githubUsername = submission.user.login
+    let studentName = students.find(student => student.username == githubUsername).name
+    let repoPath = submission.head.repo.full_name
+    let orgName = submission.base.user.login
 
     cliCommand += ` && hw ${studentName} ${repoPath} ${orgName}`
-  }) 
+  })
 
-  const childProcess = spawn(cliCommand, { shell: true }) 
+  const childProcess = spawn(cliCommand, { shell: true })
 
   childProcess.stdout.on("data", data => {
-    console.log(data.toString().trim()) 
-  }) 
+    console.log(data.toString().trim())
+  })
   childProcess.stderr.on("data", data => {
-    console.error(error(data.toString().trim())) 
-  }) 
+    console.error(error(data.toString().trim()))
+  })
   const exitCode = await new Promise((resolve, reject) => {
-    childProcess.on('close', resolve) 
-  }) 
+    childProcess.on('close', resolve)
+  })
 }
 
 // print out which students didn't submit pull request submission
 function logMissingSubmissions(submissions) {
-	// log missing students to update course tracker
-	const numColor = submissions.length < students.length ? error : success
-	console.log(numColor(`${submissions.length} of ${students.length}`), `added to finished-assingments.json.`)
-	if (submissions.length !== students.length) {
-		//array of github usernames that made submission
-		let githubUsernames = submissions.map((submission) => submission.user.login)
-		//array of students that didnt make submission
-		let difference = students.filter((student) => !githubUsernames.includes(student.username))
+  // log missing students to update course tracker
+  const numColor = submissions.length < students.length ? error : success
+  console.log(numColor(`${submissions.length} of ${students.length}`), `added to finished-assingments.json.`)
+  if (submissions.length !== students.length) {
+    //array of github usernames that made submission
+    let githubUsernames = submissions.map((submission) => submission.user.login)
+    //array of students that didnt make submission
+    let difference = students.filter((student) => !githubUsernames.includes(student.username))
 
-		let names = ''
-		difference.forEach((student) => (names += `${student.name}\n`))
-		console.log(error(`Missing submissions from:\n${names}\n`))
-	}
+    let names = ''
+    difference.forEach((student) => (names += `${student.name}\n`))
+    console.log(error(`Missing submissions from:\n${names}\n`))
+  }
 }
 
 // adds new assignment to the finished assignments json (does not add if it is not new)
 function addNewAssigment(repoName) {
   const finishedJson = JSON.parse(readFileSync('finished-assingments.json'))
-  if(!finishedJson.assignments.includes(repoName)) {
+  if (!finishedJson.assignments.includes(repoName)) {
     finishedJson.assignments.push(repoName)
     writeFileSync('./finished-assingments.json', JSON.stringify(finishedJson))
   }
@@ -289,14 +289,14 @@ function addNewAssigment(repoName) {
 // adds assignment and student submissions to json
 function updateFinishedAssignments(submissions, repoName) {
   //array of github usernames that made submission
-  const githubUsernames = submissions.map(submission => submission.user.login) 
+  const githubUsernames = submissions.map(submission => submission.user.login)
   const finishedJson = JSON.parse(readFileSync('finished-assingments.json'))
   finishedJson.students.forEach(student => {
     // only add if a submission is found
-    if(githubUsernames.includes(student.username)){
+    if (githubUsernames.includes(student.username)) {
       // do not add duplicates
-      if(!student.completed.includes(repoName)) student.completed.push(repoName)
-    } 
+      if (!student.completed.includes(repoName)) student.completed.push(repoName)
+    }
   })
   writeFileSync('./finished-assingments.json', JSON.stringify(finishedJson))
 }
@@ -315,9 +315,9 @@ function createFinishedJson() {
 // promisify the spawn untility
 function spawnAsync(command) {
   return new Promise((resolve, reject) => {
-    const childProcess = spawn(command, { shell: true }) 
-    childProcess.stdout.on('data', data => console.log(data.toString().trim())) 
-    childProcess.stderr.on('data', data => reject(error(data.toString().trim()))) 
+    const childProcess = spawn(command, { shell: true })
+    childProcess.stdout.on('data', data => console.log(data.toString().trim()))
+    childProcess.stderr.on('data', data => reject(error(data.toString().trim())))
     childProcess.on('close', code => resolve(code))
   })
 }
@@ -329,7 +329,7 @@ function spawnAsync(command) {
  */
 
 // --check: checks submissions in finished-assignments.json
-function checkSubmissions() { 
+function checkSubmissions() {
   const finishedJson = JSON.parse(readFileSync('finished-assingments.json'))
   // less than or equal to this will appear yellow 
   const yellowPercent = 85
@@ -342,7 +342,7 @@ function checkSubmissions() {
     const missing = []
     let missingString = ''
     finishedJson.assignments.forEach(assignment => {
-      if(!student.completed.includes(assignment)) {
+      if (!student.completed.includes(assignment)) {
         missing.push(assignment)
         // FIXME: If used with more than one organization, works well for seir-flex-831
         const organization = orgs[0]
@@ -352,12 +352,12 @@ function checkSubmissions() {
     // calculate missing percentage
     const percent = Math.round((finishedJson.assignments.length - missing.length) * 100 / finishedJson.assignments.length)
     // print student name and missing assigments
-    const color = percent < redPercent ? error : 
-                  percent <= yellowPercent ? warn :
-                  success 
+    const color = percent < redPercent ? error :
+      percent <= yellowPercent ? warn :
+        success
     const msg = `${student.name}\n${missing.length} missed assignments:\n${missingString}\ncompletion rate: ${percent}%`
-    if(flags.includes('--noGreen')) {
-      if(percent < yellowPercent) {
+    if (flags.includes('--noGreen')) {
+      if (percent < yellowPercent) {
         noWarns = false
         console.log('-----\n')
         console.log(color(msg))
@@ -371,14 +371,14 @@ function checkSubmissions() {
     }
   })
   // message if no warnings where found
-  if(noWarns) console.log(success('Only green students found!'))
+  if (noWarns) console.log(success('Only green students found!'))
 }
 
 // --forget: removes the supplied repo from the list of assignments
-function forgetRepo(repoName) { 
+function forgetRepo(repoName) {
   const finishedJson = JSON.parse(readFileSync('finished-assingments.json'))
   // end early if assignment not tracked
-  if(!finishedJson.assignments.includes(repoName)) return console.log(error(`assignment`), info(repoName), error('not currently tracked'))
+  if (!finishedJson.assignments.includes(repoName)) return console.log(error(`assignment`), info(repoName), error('not currently tracked'))
   // remove all references to the unwanted assignment
   finishedJson.assignments = finishedJson.assignments.filter(assignment => assignment !== repoName)
   finishedJson.students = finishedJson.students.map(student => {
@@ -389,12 +389,12 @@ function forgetRepo(repoName) {
   })
   // write the json and remove the directory
   writeFileSync('./finished-assingments.json', JSON.stringify(finishedJson))
-  spawn(`rm -rf ${repoName}`, { shell: true }) 
+  spawn(`rm -rf ${repoName}`, { shell: true })
   console.log(success('removed assignment:'), info(repoName), success('from being tracked'))
 }
 
 // --list: print out all tracked assignments
-function listAssignments() { 
+function listAssignments() {
   const finishedJson = JSON.parse(readFileSync('finished-assingments.json'))
   console.log(info('currently tracking:'))
   finishedJson.assignments.forEach(assignment => console.log(assignment))
@@ -402,55 +402,55 @@ function listAssignments() {
 
 // --sync: looks at the config.json and adds any new students to the finished assignments json
 function syncStudents() {
-	const finishedJson = JSON.parse(readFileSync('finished-assingments.json'))
-	// find all the new students added to the config
-	const newStudents = students.filter(student => {
+  const finishedJson = JSON.parse(readFileSync('finished-assingments.json'))
+  // find all the new students added to the config
+  const newStudents = students.filter(student => {
     let isNew = true
     finishedJson.students.forEach(finStudent => {
-      if(finStudent.name === student.name) isNew = false
+      if (finStudent.name === student.name) isNew = false
     })
     return isNew
   })
   newStudents.forEach(newStudent => {
-    finishedJson.students.push({...newStudent, completed: []})
-    console.log(info(`added:`),newStudent.name)
+    finishedJson.students.push({ ...newStudent, completed: [] })
+    console.log(info(`added:`), newStudent.name)
   })
 
   // find all the students who have been taken out of the config and remove them
-	const dropStudents = finishedJson.students.filter(student => {
+  const dropStudents = finishedJson.students.filter(student => {
     let isDrop = true
     students.forEach(confStudent => {
-      if(confStudent.name === student.name) isDrop = false
+      if (confStudent.name === student.name) isDrop = false
     })
     return isDrop
   })
   // remove any students that may have dropped
   dropStudents.forEach(dropStudent => {
-    finishedJson.students = finishedJson.students.filter(currentStudent =>{
-      if(dropStudent.name != currentStudent.name) {
+    finishedJson.students = finishedJson.students.filter(currentStudent => {
+      if (dropStudent.name != currentStudent.name) {
         return true
       } else {
         console.log(error('removing'), currentStudent.name)
         return false
       }
-    }) 
+    })
   })
-	writeFileSync('./finished-assingments.json', JSON.stringify(finishedJson))
+  writeFileSync('./finished-assingments.json', JSON.stringify(finishedJson))
 }
 
 // --updateAll: loops over the array of finished assigments and reclones them all
 async function updateAll() {
   console.log(info('Updating all repos found in the ./finished-assignments.json'))
   if (!existsSync('./finished-assingments.json')) {
-		console.log(warn('./finished-assignments.json not found. Make sure to clone a homework first.'))
-	}
+    console.log(warn('./finished-assignments.json not found. Make sure to clone a homework first.'))
+  }
   const { assignments } = JSON.parse(readFileSync('finished-assingments.json'))
   // remove overwrite flag to avoid infinite loop
   const filteredFlags = flags.filter(flag => flag === '--overWrite')
   // build command from flags and execute it
   const updateCommands = assignments.map((assignment) => {
     let command = `node cloneHw.js ${assignment}`
-    filteredFlags.forEach(flag =>  command += ` ${flag}`)
+    filteredFlags.forEach(flag => command += ` ${flag}`)
     return command
   })
 
@@ -516,10 +516,10 @@ function allCompleted(repoName) {
 
   // add assignment to each student
   finishedJson.students.forEach(student => {
-      if (!student.completed.includes(repoName)) {
-          console.log(info(`adding ${repoName} as completed for ${student.name}`))
-          student.completed.push(repoName)
-      }
+    if (!student.completed.includes(repoName)) {
+      console.log(info(`adding ${repoName} as completed for ${student.name}`))
+      student.completed.push(repoName)
+    }
   })
 
   writeFileSync('./finished-assingments.json', JSON.stringify(finishedJson))
